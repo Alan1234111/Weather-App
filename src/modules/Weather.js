33c9e664bd65f145c0abddedd0a00cc6e7440ff5
Alan Pason <alan.pason132@gmail.com>
2023-03-18 20:24:33 +0100

    Feat: after submit form weather details show up

--- src/modules/Weather.js
+++ src/modules/Weather.js
@@ -2,6 +2,15 @@ import HandleApi from "./HandleApi";
 
 export default class Weather {
   constructor() {
+    this.locationName = document.getElementById("location-name");
+    this.locationTime = document.getElementById("location-time");
+    this.weatherTemp = document.getElementById("weather-temp");
+    this.weatherName = document.getElementById("weather-name");
+    this.weatherIcon = document.getElementById("weather-icon");
+    this.weatherWind = document.getElementById("weather-wind");
+    this.weatherHumidity = document.getElementById("weather-humidity");
+    this.weatherPressure = document.getElementById("weather-pressure");
+
     this.handleApi = new HandleApi();
     this.formSearch = document.getElementById("form-location");
     this.formSearch.addEventListener("submit", this.getWeatherDetails);
@@ -12,16 +21,23 @@ export default class Weather {
     const location = document.getElementById("input-location").value;
     const formatLocation = this.formatLocationText(location);
     this.showWeather(this.handleApi.getWeather(formatLocation));
+    localStorage.setItem("lastLocation", formatLocation);
   };
 
   async showWeather(weatherDetails) {
     const response = await weatherDetails;
-    console.log(response);
+    this.locationName.textContent = response.location;
+    this.locationTime.textContent = response.time;
+    this.weatherTemp.textContent = `${response.temp} °C`;
+    this.weatherName.textContent = response.weatherName;
+    this.weatherIcon.src = response.weatherIcon;
+    this.weatherWind.textContent = `${response.windSpeed} km/h`;
+    this.weatherHumidity.textContent = `${response.humidity} %`;
+    this.weatherPressure.textContent = `${response.pressure} hpa`;
   }
 
   formatLocationText(location) {
     let formatLocation = location.trim().toLowerCase();
-
     return formatLocation;
   }
 }
