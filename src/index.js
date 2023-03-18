515b819afdff0acf057a09f74292cfb90407461d
Alan Pason <alan.pason132@gmail.com>
2023-03-18 20:53:27 +0100

    Feat: show last searched location after refresh page

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import Weather from "./modules/Weather";
 
 function init() {
   const weather = new Weather();
+
+  if (localStorage.getItem("lastLocation")) {
+    weather.showWeather(weather.handleApi.getWeather(localStorage.getItem("lastLocation")));
+  } else {
+    weather.showWeather(weather.handleApi.getWeather("london"));
+  }
 }
+
 init();
+
+// GUVLXAK3DHQBXF1NGNAG605XOZFXFO6R
