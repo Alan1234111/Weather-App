90ebaab6ed546926e321e7ee39cd5ec6a48e847a
Alan Pason <alan.pason132@gmail.com>
2023-03-18 22:40:38 +0100

    Add: cors and try catch block

--- src/modules/HandleApi.js
+++ src/modules/HandleApi.js
@@ -1,19 +1,23 @@
 export default class HandleApi {
   async getWeather(location) {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ef42a1388a804e28b3d173616231803&q=${location}`);
-    const weatherData = await response.json();
+    try {
+      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ef42a1388a804e28b3d173616231803&q=${location}`, {mode: "cors"});
+      const weatherData = await response.json();
 
-    const weatherDetails = {};
+      const weatherDetails = {};
 
-    weatherDetails.location = weatherData.location.name;
-    weatherDetails.time = weatherData.location.localtime.split(" ").slice(1)[0];
-    weatherDetails.weatherIcon = weatherData.current.condition.icon;
-    weatherDetails.weatherName = weatherData.current.condition.text;
-    weatherDetails.temp = weatherData.current.feelslike_c;
-    weatherDetails.windSpeed = weatherData.current.wind_kph;
-    weatherDetails.humidity = weatherData.current.humidity;
-    weatherDetails.pressure = weatherData.current.pressure_mb;
+      weatherDetails.location = weatherData.location.name;
+      weatherDetails.time = weatherData.location.localtime.split(" ").slice(1)[0];
+      weatherDetails.weatherIcon = weatherData.current.condition.icon;
+      weatherDetails.weatherName = weatherData.current.condition.text;
+      weatherDetails.temp = weatherData.current.feelslike_c;
+      weatherDetails.windSpeed = weatherData.current.wind_kph;
+      weatherDetails.humidity = weatherData.current.humidity;
+      weatherDetails.pressure = weatherData.current.pressure_mb;
 
-    return weatherDetails;
+      return weatherDetails;
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
